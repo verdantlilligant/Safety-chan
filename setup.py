247037d61d5b5a2081b4dc64ca0725513b584393
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2020-03-14 08:15:51 -0400

    package some things

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='Safety-chan',
+   version='1.0',
+   description='Safety Third Discord Bot',
+   author='Kendall Garner',
+   packages=['bot'],
+   install_requires=['discord.py']
+)
