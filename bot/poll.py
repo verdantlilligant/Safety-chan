77c17a5900fc5a15cbb1f44fd4c45b9ed087111d
Kendall Garner <someuser@trash.localdomain>
2020-03-22 13:59:07 -0400

    Fix polls and admin

--- bot/poll.py
+++ bot/poll.py
@@ -1,20 +1,32 @@
+from asyncio import gather, sleep
+from discord import Message
 from discord.ext import commands
-from discord.ext.commands import Cog, Context, command, CommandInvokeError
+from discord.ext.commands import Bot, Cog, Context, command, CommandInvokeError
 from discord.utils import get
+from time import time
+
+from typing import List, Tuple
 
 emojis_order = [
   "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"
 ]
 
-class PollManager(Cog):
-  def __init__(self, bot):
-    self.bot = bot
+def vote_str(count: int) -> str:
+  return "vote" if count == 1 else "votes"
 
+class PollManager(Cog):
+  def __init__(self, bot: Bot):
+    self.bot = bot
+    self.polls: List[Tuple[int, float, Message]] = []
+    
   async def cog_command_error(self, ctx: Context, error: CommandInvokeError):
     """
     Handles errors for poll commands
     """
     await ctx.send(error.original)
+
+  def should_remove(self, old_time: float, current_time: float, minutes: int) -> bool:
+    return current_time - old_time >= minutes * 10
 
   @commands.command()
   async def poll(self, ctx, topic: str, timing: int, *options):
@@ -47,7 +59,7 @@ class PollManager(Cog):
 
     time_msg = "minutes" if timing > 1 else "minute"
 
-    poll = f"Poll by {ctx.message.author.mention} ({timing} {time_msg}): **{topic}**\n\n```"
+    poll = f"poll by {ctx.message.author.mention} ({timing} {time_msg}): **{topic}**\n\n```\n"
 
     for idx in range(len(options)):
       poll += f"{idx + 1}. {options[idx]}\n"
@@ -56,3 +68,51 @@ class PollManager(Cog):
 
     for idx in range(len(options)):
       await message.add_reaction(emojis_order[idx])
+
+    await sleep(timing * 60)
+
+    msg = await message.channel.fetch_message(message.id)
+    
+    lines = msg.content.split("\n")
+  
+    # remove the leading numbers (1., 10.)
+    options = [
+      line[line.index(".") + 2:] for line in lines[3:-1]
+    ]
+
+    results: List[Tuple[int, str]] = []
+
+    for reaction in msg.reactions:
+      try:
+        index = emojis_order.index(reaction.emoji)
+        results.append((reaction.count - 1, options[index]))
+      except ValueError:
+        pass
+
+    results.sort(reverse=True)
+    
+    wins = [results[0][1]]
+    max_count = results[0][0]
+
+    for idx in range(1, len(results)):
+      if results[idx][0] == max_count:
+        wins.append(results[idx][1])
+      else:
+        break
+    
+    wins.sort()
+
+    max_vote_msg = vote_str(max_count)
+    result_msg = f"results of {lines[0]}:\n"
+
+    if len(wins) > 1:
+      joined_str = ", ".join(wins)
+      result_msg += f"**Tie between {joined_str}** ({max_count} {max_vote_msg} each)\n\n>>> "
+    else:
+      result_msg += f"**{wins[0]}** wins! ({max_vote_msg} votes)\n\n>>> "
+
+    for idx in range(len(wins), len(results)):
+      vote_msg = vote_str(results[idx][0])
+      result_msg += f"**{results[idx][1]}** ({results[idx][0]} {vote_msg})\n"
+
+    await ctx.send(result_msg)
