3e18dcae29694bf71ddecbd3e7b1ec68bbc1de40
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2020-03-26 00:50:11 -0400

    birthdays?

--- bot/bot.py
+++ bot/bot.py
@@ -1,12 +1,14 @@
 from discord import Member, Role, User
 from discord.ext.commands import Bot, CommandInvokeError, DefaultHelpCommand, Context, Converter, Greedy
 
+from .birthdays import BirthdayManager
 from .poll import PollManager
 from .roles import RolesManager
 from .roll import RollManager
 
 bot = Bot(command_prefix='>', help_command=DefaultHelpCommand(dm_help=True))
 
+bot.add_cog(BirthdayManager(bot))
 bot.add_cog(PollManager(bot))
 bot.add_cog(RolesManager(bot))
 bot.add_cog(RollManager(bot))
