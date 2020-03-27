f15c2261447e0e051eba1e09850c373639ceaae0
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2020-03-27 15:48:12 -0400

    document events, add cancel

--- bot/util.py
+++ bot/util.py
@@ -17,6 +17,17 @@ us_timezones = {
 }
 
 def get_date(input: str, formats: List[str]) -> Optional[datetime]:
+  """
+  Parses a date given a list of possible string formats.
+  The formats are parsed from left to right, accepting the first format that works.
+
+  Args:
+    input (str): the string to parse
+    formats (List[str]): a list of time formats
+
+  Returns:
+    a datetime representing the time, or None if all fo the formats fail
+  """
   for format in formats:
     try:
       return datetime.strptime(input, format)
@@ -26,6 +37,17 @@ def get_date(input: str, formats: List[str]) -> Optional[datetime]:
   return None
 
 def get_local_date(input: str) -> Optional[datetime]:
+  """
+  Parses a date string with a time zone and converts it to server local time.
+  If the string cannot be parsed, returns None
+
+  Args:
+    input (str): an input string with time zone string (%Z)
+
+  Returns:
+    a datetime representing the time converted to server local time
+    or None if the parse fails
+  """
   try:
     return parse(input, tzinfos=us_timezones).astimezone(tzlocal())
   except:
