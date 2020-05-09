5c3995a33708fb2d316f211faa8ec6cf75fe560a
Kendall Garner <kgarner@mit.edu>
2020-05-09 16:46:11 -0400

    add impersonation feature

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,7 @@
 from discord import Member, Role, User
 from discord.ext.commands import Bot, CommandInvokeError, DefaultHelpCommand, Context, Converter, Greedy
 
-from .cogs import BirthdayManager, EventsManager, PollManager, RolesManager, RollManager, StatusManager
+from .cogs import BirthdayManager, EventsManager, ImpersonateManager, PollManager, RolesManager, RollManager, StatusManager
 
 __all__ = ["bot"]
 
@@ -9,6 +9,7 @@ bot = Bot(command_prefix='>', help_command=DefaultHelpCommand(dm_help=True))
 
 bot.add_cog(BirthdayManager(bot))
 bot.add_cog(EventsManager(bot))
+bot.add_cog(ImpersonateManager(bot))
 bot.add_cog(PollManager(bot))
 bot.add_cog(RolesManager(bot))
 bot.add_cog(RollManager(bot))
---
+++ bot/cogs/impersonate.py
@@ -0,0 +1,13 @@
+from discord import TextChannel
+from discord.ext.commands import Bot, Cog, Context, command, is_owner
+
+__all__ = ["ImpersonateManager"]
+
+class ImpersonateManager(Cog):
+  def __init__(self, bot: Bot):
+    self.bot = bot
+
+  @is_owner()
+  @command()
+  async def impersonate(self, ctx: Context, channel: TextChannel, msg: str):
+    await channel.send(f"```{msg}```")
